b4dde7104e16d83034cc98073cc5f8b43551c04f
Haggai Alehegn <111057970+haggai333@users.noreply.github.com>
2026-01-22 22:19:10 +0300

    Time: 23 ms (12.66%), Space: 12.5 MB (53.91%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -2,13 +2,13 @@ class Solution(object):
     def isPalindrome(self, x):
         a=0
         b=x
-          
-            
-        while(b>1):
-            a=(10*a)+(b/10)
-            b=b/10
          
-        print(a) 
+            
+        while(b>0):
+            a=(10*a)+(b%10)
+            b=b//10
+         
+        
         return a==x    
             
         
