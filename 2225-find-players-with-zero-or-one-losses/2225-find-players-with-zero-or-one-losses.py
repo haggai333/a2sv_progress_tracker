8cdb92f4f280bbfe649e13a08d07edee5aea86f9
Haggai Alehegn <111057970+haggai333@users.noreply.github.com>
2026-01-27 19:51:04 +0300

    Time: 134 ms (47.69%), Space: 60.7 MB (46.69%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
@@ -5,10 +5,10 @@ class Solution:
         for i in matches:
             if i[0] not in rank:
                 rank[i[0]]=0
-            if i[1] in rank:
-                rank[i[1]]-=1
-            else:
+            if i[1] not in rank:
                 rank[i[1]]=-1
+                continue
+            rank[i[1]]-=1
         
         for j in rank:
             if rank[j]==0:
