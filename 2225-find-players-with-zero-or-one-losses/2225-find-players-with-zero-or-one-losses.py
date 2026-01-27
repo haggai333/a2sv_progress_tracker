25a3dded8a98703241f24ba9d47fd79b988a0289
Haggai Alehegn <111057970+haggai333@users.noreply.github.com>
2026-01-27 19:50:46 +0300

    Time: 123 ms (63.07%), Space: 60.6 MB (55.75%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
@@ -1,8 +1,7 @@
 class Solution:
     def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
         rank={}
-        answer=[]
-        track=0
+        answer=[[],[]]
         for i in matches:
             if i[0] not in rank:
                 rank[i[0]]=0
@@ -10,12 +9,13 @@ class Solution:
                 rank[i[1]]-=1
             else:
                 rank[i[1]]=-1
-        for i in range(2):
-            temp=[]
-            for j in rank:
-                if i==0 and rank[j]==0:
-                    temp.append(j)
-                if rank[j]==-1 and i==1:
-                    temp.append(j)
-            answer.append(sorted(temp))
+        
+        for j in rank:
+            if rank[j]==0:
+                answer[0].append(j)
+            if rank[j]==-1:
+                answer[1].append(j)
+        answer[0].sort()
+        answer[1].sort()
+            
         return answer
