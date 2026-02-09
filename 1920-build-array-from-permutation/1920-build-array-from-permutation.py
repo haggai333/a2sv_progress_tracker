af24069d7eb4f9c88fabbd82ab49ef470d9047b4
Haggai Alehegn <111057970+haggai333@users.noreply.github.com>
2026-02-09 22:05:29 +0300

    Time: 0 ms (100%), Space: 19.4 MB (44.65%) - LeetHub

---
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.py
@@ -0,0 +1,7 @@
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        answer=[]
+        for i in nums:
+            
+            answer.append(nums[i])
+        return answer
