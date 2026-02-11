7c99aae8995f367e504e9eb38a9cf07d7d4a5692
Haggai Alehegn <111057970+haggai333@users.noreply.github.com>
2026-02-11 19:15:40 +0300

    Time: 3 ms (12.56%), Space: 19.6 MB (15.08%) - LeetHub

--- 0789-escape-the-ghosts/0789-escape-the-ghosts.py
+++ 0789-escape-the-ghosts/0789-escape-the-ghosts.py
@@ -7,7 +7,7 @@ class Solution:
             ghostdistance.append([abs(i[0]-target[0])+abs(i[1]-target[1]),abs(i[0]+i[1])])
         print(ghostdistance)
         for i in ghostdistance:
-            if i[0]<=mydistance or i[1]<mydistance:
+            if i[0]<=mydistance :
                 return False
         return True
         
