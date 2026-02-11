b2bcce661a403fdeac964f52091b38654c9410b4
Haggai Alehegn <111057970+haggai333@users.noreply.github.com>
2026-02-11 19:16:07 +0300

    Time: 0 ms (100%), Space: 19.4 MB (15.08%) - LeetHub

--- 0789-escape-the-ghosts/0789-escape-the-ghosts.py
+++ 0789-escape-the-ghosts/0789-escape-the-ghosts.py
@@ -2,12 +2,8 @@ class Solution:
     def escapeGhosts(self, ghosts: List[List[int]], target: List[int]) -> bool:
         ghostdistance=[]
         mydistance=abs(target[0])+abs(target[1])
-        print(mydistance)
         for i in ghosts:
-            ghostdistance.append([abs(i[0]-target[0])+abs(i[1]-target[1]),abs(i[0]+i[1])])
-        print(ghostdistance)
-        for i in ghostdistance:
-            if i[0]<=mydistance :
-                return False
+           if abs(i[0]-target[0])+abs(i[1]-target[1])<=mydistance:
+            return False
         return True
         
