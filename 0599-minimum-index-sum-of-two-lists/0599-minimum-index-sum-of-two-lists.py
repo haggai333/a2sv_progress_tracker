21ced4acff17730503ae7c443e231729e0b5a125
Haggai Alehegn <111057970+haggai333@users.noreply.github.com>
2026-02-11 17:54:37 +0300

    Time: 199 ms (15.6%), Space: 19.9 MB (19.89%) - LeetHub

--- 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.py
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.py
@@ -3,15 +3,19 @@ class Solution:
         answer=[]
         tempdic={}
         mini=99999
-        for i in list1:
-            if i in list1 and i in list2 and i not in tempdic:
-                tempdic[i]=list1.index(i)+list2.index(i)
+        for i in range(len(list1)):
+            if list1[i] in list1 and list1[i] in list2 and list1[i] not in tempdic:
+                tempdic[list1[i]]=i
+        for i in range(len(list2)):
+            if list2[i] in list1 and list2[i] in list2 :
+                tempdic[list2[i]]+=i
+        
         for i in tempdic:
             mini=min(mini,tempdic[i])
         for i in tempdic:
             if tempdic[i]==mini:
                 answer.append(i)
-        return answer
+        return list(set(answer))
 
 
 
