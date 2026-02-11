3f068f5501e69fddc61149164fdca369ec033263
Haggai Alehegn <haggaialehegn12@gmail.com>
2026-02-11 19:48:39 +0300

    fix

--- A_Soldier_and_Bananas.py
+++ A_Soldier_and_Bananas.py
@@ -2,5 +2,8 @@ lists=list(map(int,input().split()))
 totalamount=0
 for i in range(1,lists[2]+1):
     totalamount+=lists[0]*i
-print(totalamount-lists[1])
-
+result=totalamount-lists[1]
+if result>0:
+    print(result)
+else:
+    print(0)
