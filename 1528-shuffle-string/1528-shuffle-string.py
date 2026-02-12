e515fbae21afe0028bda805e336c54928b93d4e8
Haggai Alehegn <111057970+haggai333@users.noreply.github.com>
2026-02-12 23:12:14 +0300

    Time: 2 ms (35.35%), Space: 19.3 MB (63.15%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.py
+++ 1528-shuffle-string/1528-shuffle-string.py
@@ -1,11 +1,11 @@
 class Solution:
     def restoreString(self, s: str, indices: List[int]) -> str:
         mapans={}
-        
-        for i in range(len(s)):
+        size=len(s)
+        for i in range(size):
             mapans[indices[i]]=s[i]
         answer=""
-        for i in range(len(s)):
+        for i in range(size):
             answer+=str(mapans[i])
         return answer
 
