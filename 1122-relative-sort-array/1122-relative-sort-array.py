5c54073ed0375fe8e170bbd51f57959d33107c87
Haggai Alehegn <111057970+haggai333@users.noreply.github.com>
2026-02-26 22:50:57 +0300

    Time: 3 ms (39.82%), Space: 19.4 MB (47.94%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.py
+++ 1122-relative-sort-array/1122-relative-sort-array.py
@@ -1,12 +1,5 @@
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
-        counter={}
-        for i in arr1:
-            if i in counter:
-                counter[i]+=1
-            else:
-                counter[i]=1
-        
         arr1.sort(key=lambda x: (arr2.index(x) if x in arr2 else 100000 + x))
         return arr1
 
