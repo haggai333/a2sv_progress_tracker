d7b9c6dcc78619c226bc9cbd05d504ec7f375a02
Haggai Alehegn <111057970+haggai333@users.noreply.github.com>
2026-03-01 22:43:07 +0300

    Time: 0 ms (100%), Space: 19.1 MB (92.98%) - LeetHub

--- 2165-smallest-value-of-the-rearranged-number/2165-smallest-value-of-the-rearranged-number.py
+++ 2165-smallest-value-of-the-rearranged-number/2165-smallest-value-of-the-rearranged-number.py
@@ -2,18 +2,21 @@ class Solution:
     def smallestNumber(self, num: int) -> int:
         a=str(num)
         l=list(a.strip())
+        
         if num>0:
             l.sort()
         else:
             l.sort(reverse=True)
         if '-' in l:
             l.remove('-')  
-        if num>0:
+        
+        if num>0 and '0' in l:
             for i in range(len(l)):
                 if int(l[i])>0:
                     l[0]=l[i]
                     l[i]='0'
                     break   
+        
         a="".join(l)   
         if num>0:
             return int(a)
