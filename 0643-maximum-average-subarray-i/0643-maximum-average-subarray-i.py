8aa3ff7d388255a4046b60994ddbc93491ab83e6
Haggai Alehegn <111057970+haggai333@users.noreply.github.com>
2026-03-10 19:54:33 +0300

    Time: 1478 ms (5.02%), Space: 18.3 MB (98.89%) - LeetHub

---
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def findMaxAverage(self, nums, k):
+        count=0
+        l=0
+        r=0
+        size=len(nums)
+        if k==1:
+            return max(nums)
+        r=k-1
+        for i in range(0,k):
+            count+=nums[i]
+        r+=1
+        sum=count
+        while r<size:
+            count+=(nums[r]-nums[l])
+            sum=max(sum,count)
+            print(sum)
+            r+=1
+            l+=1
+        print(sum)
+        return float(float(sum)/float(k))
