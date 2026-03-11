c341c62a5afb0e78a05fac597a0bf15fedadc508
Haggai Alehegn <111057970+haggai333@users.noreply.github.com>
2026-03-11 21:33:32 +0300

    Time: 54 ms (54.66%), Space: 13.3 MB (86.02%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,21 +1,32 @@
 class Solution(object):
     def findAnagrams(self, s, p):
-        checker=set(p)
-        check=set()
+        checker={}
+        check={}
+        for i in p:
+            if i in checker:
+                checker[i]+=1
+            else:
+                checker[i]=1
         l=0
         r=0
         answer=[]
         while r<len(s):
-            check.add(s[r])
+            if s[r] in check:
+                check[s[r]]+=1
+            else:
+                check[s[r]]=1
             r+=1
-            if r-l==len(checker):
+
+            if r-l==len(p):
                 if check==checker:
                     answer.append(l)
-                if s[l]==s[r-1]:
-                    l+=1
-                    continue
-                check.discard(s[l])
+                if s[l] in check:
+                    if check[s[l]]>1:
+                        check[s[l]]-=1
+                    else:
+                        del check[s[l]]
                 l+=1
+            
         return answer
 
 
