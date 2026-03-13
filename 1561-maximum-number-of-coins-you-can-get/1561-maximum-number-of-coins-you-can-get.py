b61cb4e6113b736e924080bca10726e1e4b32239
Haggai Alehegn <111057970+haggai333@users.noreply.github.com>
2026-03-13 18:21:56 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.py
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.py
@@ -2,17 +2,8 @@ class Solution(object):
     def maxCoins(self, piles):
         piles.sort(reverse=True)
         result=0
-        canpick=len(piles)/3
-        count=0
-        for i in range(1,len(piles),2):
+        canpick=((len(piles)/3)*2)+1
+        for i in range(1,canpick,2):
             result+=piles[i]
-            count+=1
-            if canpick==count:
-                break
-
-        
-        
-            
         return result
-
         
