ad8f68ceda58a80a9ef63e5e677f38761b78e53e
Haggai Alehegn <111057970+haggai333@users.noreply.github.com>
2026-03-13 21:24:09 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
@@ -4,18 +4,16 @@ class Solution(object):
             return sum(cardPoints)
         sums=sum(cardPoints)
         windowsize=len(cardPoints)-k
-        print(windowsize)
         l=0
         tempsum=0
         answer=0
         for r in range(len(cardPoints)):
             tempsum+=cardPoints[r]
             if r-l==windowsize-1:
-                
                 answer=max(answer,sums-tempsum)
                 tempsum-=cardPoints[l]
                 l+=1
-                print(l,r,tempsum,answer)
+               
         return answer
                 
 
