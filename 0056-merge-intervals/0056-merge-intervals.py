9f8688472c4e49cfffc1f947933cca028f80a82a
Haggai Alehegn <111057970+haggai333@users.noreply.github.com>
2026-03-13 14:18:21 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,14 +1,24 @@
 class Solution(object):
     def merge(self, intervals):
         intervals.sort(key=lambda x: x[0])
-        merged = []
-        for interval in intervals:
-            if not merged or merged[-1][1] < interval[0]:
-                merged.append(interval)
-            else:
-                merged[-1][1] = max(merged[-1][1], interval[1])
+        temp=[]
+        for i in intervals:
+            temp.append(i[0])
+            temp.append(i[1])
+        print temp
+        answer=[]
+        l=0
+        for i in range(1,len(temp)-1):
+            if i%2==0 and i>0:
+                if temp[i]>temp[i-1]  and temp[i]>=temp[i+1]:
+                    continue
+                else:
+                    answer.append([temp[l],temp[i+1]])
+                    l=i+2
+            
+        return answer
 
-        return merged
+
 
 
         
