0168d02e2af056115889489b2dd6df5f056326e2
Haggai Alehegn <111057970+haggai333@users.noreply.github.com>
2026-03-16 23:13:10 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.py
+++ 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.py
@@ -1,19 +1,15 @@
 class Solution(object):
     def maxTurbulenceSize(self, arr):
         shift=""
-        window={}
         l=0
         answer=0
         if len(set(arr))==1:
             return 1
         for r in range(1,len(arr)):
-            window[arr[r-1]]=1+window.get(arr[r-1],0)
             
             if arr[r]>arr[r-1]:
                 if shift=="r":
-                    while r-1>l:
-                        arr[l]-=1
-                        l+=1
+                        l=r-1
 
                 else:
                     shift="r"
@@ -22,20 +18,17 @@ class Solution(object):
 
             elif arr[r]<arr[r-1]:
                 if shift=="l":
-                    while r-1>l:
-                        arr[l]-=1
-                        l+=1
+                    
+                        l=r-1
 
                 else:
                     shift="l"
                    
                     answer=max(answer,r-l+1)
             else:
-                
-                while r>l:
-                        arr[l]-=1
-                        l+=1
+                        l=r
         return answer
+
 
 
         
