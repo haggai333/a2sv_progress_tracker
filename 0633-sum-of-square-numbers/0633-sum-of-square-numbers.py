53b56e7b87d3b0bdad4c13095ea50e9e7cef820d
Haggai Alehegn <111057970+haggai333@users.noreply.github.com>
2026-03-20 20:58:33 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -1,16 +1,11 @@
 class Solution(object):
     def judgeSquareSum(self, c):
         r=int(sqrt(c))
-        temp=[]
-        for i in range(r+1):
-            temp.append(i*i)
         l=0
-        
-        r=len(temp)-1
         while(r>=l):
-            if temp[r]+temp[l]<c:
+            if (r*r)+(l*l)<c:
                 l+=1
-            elif temp[r]+temp[l]>c:
+            elif (r*r)+(l*l)>c:
                 r-=1
             else:
                 return True
