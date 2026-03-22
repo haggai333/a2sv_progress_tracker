b6af3c88b09eea349236a04ed10275f00d3fcb08
Haggai Alehegn <haggaialehegn12@gmail.com>
2026-03-22 23:22:20 +0300

    code force

--- A_Segment_with_Small_Sum.py
+++ A_Segment_with_Small_Sum.py
@@ -2,19 +2,16 @@ size,winsize=map(int,input().split())
 arr=list(map(int,input().split()))
 l=0
 r=0
-arr.sort()
 sum=0
 length=0
 
-while r<size:
-    if sum==winsize:
-        length=max(length,r-l+1)
-    if sum>=winsize:
+for r in range(size):
+    sum+=arr[r]
+    while sum>winsize:
         sum-=arr[l]
         l+=1
-    if sum<winsize:
-        sum+=arr[r]
-        r+=1
+    length=max(length,r-l+1)
+
 print(length)
 
 
