a8819a86cd23a9fbb653020afe14130bc8c53914
Haggai Alehegn <haggaialehegn12@gmail.com>
2026-03-22 22:51:44 +0300

    codeforce

---
+++ C_Less_or_Equal.py
@@ -0,0 +1,19 @@
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+
+arr.sort()
+
+if k == 0:
+    if arr[0] == 1:
+        print(-1)
+    else:
+        print(arr[0] - 1)
+
+elif k == n:
+    print(arr[n - 1])
+
+else:
+    if arr[k - 1] < arr[k]:
+        print(arr[k - 1])
+    else:
+        print(-1)
