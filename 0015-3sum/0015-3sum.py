9577343a42c2724ad19828af92edaf1092b3bc84
Haggai Alehegn <111057970+haggai333@users.noreply.github.com>
2026-03-23 23:58:15 +0300

    Time: 714 ms (76.58%), Space: 18.3 MB (87.31%) - LeetHub

---
+++ 0015-3sum/0015-3sum.py
@@ -0,0 +1,48 @@
+class Solution(object):
+    def threeSum(self, nums):
+        nums.sort()
+        print(nums)
+        answer=[]
+        
+        for l,val in enumerate(nums):
+            if l > 0 and nums[l] == nums[l - 1]:
+                continue
+            li = l + 1
+            ri=len(nums)-1
+            if nums[l]>0:
+                break
+            
+            while ri>li:
+                
+                if nums[ri]+nums[l]+nums[li]>0:
+                    ri-=1
+                elif nums[ri]+nums[li]+nums[l]<0:
+                    li+=1
+                else:
+                    answer.append([nums[l],nums[li],nums[ri]])
+                    li += 1
+                    ri -= 1
+
+                    while li < ri and nums[li] == nums[li - 1]:
+                        li += 1
+                    while li < ri and nums[ri] == nums[ri + 1]:
+                        ri -= 1
+        return answer
+
+
+
+        
+
+                
+
+
+        
+            
+
+
+
+                
+
+
+        
+        
