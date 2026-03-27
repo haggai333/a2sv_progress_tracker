8f8186a0bb164631305fb3623f153b27439fd529
Haggai Alehegn <111057970+haggai333@users.noreply.github.com>
2026-03-27 14:59:16 +0300

    Time: 350 ms (34.09%), Space: 12.7 MB (88.64%) - LeetHub

---
+++ 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def balancedString(self, s):
+        count={}
+        current=len(s)//4
+        answer=float('inf')
+        for i in s:
+            count[i]=1+count.get(i,0)
+        if all(count.get(c, 0) <= current for c in "QWER"):
+            return 0
+        l=0
+        r=0
+        while r<len(s):
+            count[s[r]]-=1
+            while l<=r and all(count.get(c, 0) <= current for c in "QWER"):
+                answer=min(answer,r-l+1)
+                count[s[l]]+=1
+                l+=1
+            r+=1
+        
+
+        return answer
+        
+        
