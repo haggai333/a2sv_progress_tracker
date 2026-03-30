27940464ed6afc3cfca3c8b22d0224b1c5d783ea
Haggai Alehegn <111057970+haggai333@users.noreply.github.com>
2026-03-30 23:55:54 +0300

    Time: 192 ms (40%), Space: 27.2 MB (90%) - LeetHub

--- 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.py
+++ 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.py
@@ -4,10 +4,12 @@ class Solution(object):
         nums=sorted(set(nums))
         answer=size
         r=0
-
         for l in range(len(nums)):
             while r<len(nums) and nums[r]<nums[l] + size:
                 r+=1
             answer=min(answer,size-(r-l))
         return answer
+        
+
+        
         
