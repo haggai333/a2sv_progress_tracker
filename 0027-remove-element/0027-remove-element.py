12901980b30af2e9508bd352371b990911ca7610
Haggai Alehegn <111057970+haggai333@users.noreply.github.com>
2026-03-30 23:57:51 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,14 +1,11 @@
 class Solution(object):
     def removeElement(self, nums, val):
         l=0
-        while   l< len(nums):
+        while l< len(nums):
             if nums[l]==val:
                 nums[-1],nums[l]=nums[l],nums[-1]
-                
                 nums.pop()
                 continue
-                
-                
             l+=1
-        print(nums)
+        
         
