45deeb3026c7e7de32b12e3da397af0d29513dc5
Haggai Alehegn <111057970+haggai333@users.noreply.github.com>
2026-03-31 20:02:58 +0300

    Time: 55 ms (6.21%), Space: 28.4 MB (40.11%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.py
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.py
@@ -3,7 +3,7 @@ class Solution:
         arr.sort()
         print(arr)
         arr[0]=1
-        maxi=-10000000
+        maxi=arr[0]
         for i in range(1,len(arr)):
             if abs(arr[i]-arr[i-1])>1:
                 arr[i]=arr[i-1]+1
