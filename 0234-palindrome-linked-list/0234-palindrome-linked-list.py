734223c0ef9c88ba6b8d1faf24641e14d3a3ded6
Haggai Alehegn <111057970+haggai333@users.noreply.github.com>
2026-04-02 18:00:56 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -34,6 +34,7 @@ class Solution(object):
             else:
                 b=a
                 a=a.next
+        prev.next=None
         
         for i in range(count//2):
             if head.val!=b.val:
