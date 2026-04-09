c4493fd776bd3538c1bf308ba2d110b2aabc0e1a
Haggai Alehegn <111057970+haggai333@users.noreply.github.com>
2026-04-09 21:19:22 +0300

    Time: 63 ms (19.86%), Space: 16 MB (7.88%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def nextGreaterElements(self, nums):
+        line=deque()
+        l=nums+nums
+        size=len(nums)
+        next={}
+        for i,val in enumerate(nums+nums):
+            while len(line)>0 and nums[line[-1]%size]<val:
+                next[line[-1]]=i%size
+                line.pop()
+            line.append(i)
+        answer=[]
+        for i in range(len(nums)):
+            if i not in next:
+                answer.append(-1)
+            else:
+                answer.append(nums[next[i]])
+        return answer
+        
+        
