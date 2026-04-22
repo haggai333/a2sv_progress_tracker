634dc156024fd6a3a63837ed441747d0e444bd8f
Haggai Alehegn <111057970+haggai333@users.noreply.github.com>
2026-04-22 20:34:24 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,17 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        l=0
+        r=x
+        mid=r+l
+        while mid*mid!=x and abs(x-(mid*mid))>0.1:
+            mid=(r+l)/2
+            if mid*mid>x:
+                r=mid -1
+            elif mid*mid<x:
+                l=mid +1
+            else:
+                return int(mid)
+        if round(mid)*round(mid)==x:
+            return round(mid)
+        else:
+            return int(mid)
