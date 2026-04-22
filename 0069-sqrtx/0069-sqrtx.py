a888c0021a677df7478b7f9778129d4189c12e82
Haggai Alehegn <111057970+haggai333@users.noreply.github.com>
2026-04-22 20:35:26 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -3,7 +3,7 @@ class Solution:
         l=0
         r=x
         mid=r+l
-        while mid*mid!=x and abs(x-(mid*mid))>0.1:
+        while abs(mid*mid-x)>0.1:
             mid=(r+l)/2
             if mid*mid>x:
                 r=mid -1
