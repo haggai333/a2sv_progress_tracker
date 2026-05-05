c3ec4828f7f7aad737b409d33f8f8998d88c792a
Haggai Alehegn <111057970+haggai333@users.noreply.github.com>
2026-05-05 20:14:19 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,7 +1,6 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         mini=0
-        print(mini)
         answer=0
         for i in range(1,len(prices)):
             answer=max(answer,prices[i]-prices[mini])
