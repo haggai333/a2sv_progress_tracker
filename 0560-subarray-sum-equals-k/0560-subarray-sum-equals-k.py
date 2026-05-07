be20e5a2d65fad921a72d78d0a0ec8ce04f59991
Haggai Alehegn <111057970+haggai333@users.noreply.github.com>
2026-05-07 14:43:26 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -4,16 +4,12 @@ class Solution(object):
         for i in range(1,len(nums)):
             prefix.append(prefix[-1]+nums[i])
         answer=0
-        count={}
+        count={0:1}
         for i in prefix:
-            if i==k:
-                answer+=1
             if i-k in count:
                 answer+=count[i-k]
             count[i]=1+count.get(i,0)
         return answer
-
-        print(prefix)
 
         
         
