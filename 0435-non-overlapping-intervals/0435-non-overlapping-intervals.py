e1b8fe88388e5b1d168f129805708dcc876ea12e
Haggai Alehegn <111057970+haggai333@users.noreply.github.com>
2026-05-12 21:04:31 +0300

    Time: 135 ms (69.23%), Space: 45 MB (86.19%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
@@ -3,25 +3,13 @@ class Solution(object):
         intervals.sort(key=lambda x:x[0])
         count=0
         end=intervals[0][1]
-        start=intervals[0][0]
         for i in intervals:
-            if i[1]>start and i[1]<end or start>i[0] and start<i[1] or end>i[0] and end<i[1] or i[0]<end and i[0]>start:
+            if i[0]<end:
                 count+=1
-                
-                if i[1]<end:
-                    end=i[1]
-                    start=i[0]
-
-            elif start==i[0] and end==i[1]:
-                
-                count+=1
-                if i[1]<end:
-                    end=i[1]
-                    start=i[0]
+                end=min(end,i[1])
             else:
-                start=i[0]
                 end=i[1]
-            
         return count-1
+
 
         
