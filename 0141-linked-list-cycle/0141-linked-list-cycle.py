eb8eb51bfd7bcf551d3cd340780e4cce40023f88
Haggai Alehegn <111057970+haggai333@users.noreply.github.com>
2026-05-13 13:54:29 +0300

    Time: 34 ms (74.05%), Space: 19.4 MB (63.76%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -6,14 +6,12 @@
 
 class Solution(object):
     def hasCycle(self, head):
-        fast=head
         slow=head
+        fast=head
         while fast and fast.next:
             fast=fast.next.next
             slow=slow.next
             if slow==fast:
                 return True
-            
         return False
-        
         
