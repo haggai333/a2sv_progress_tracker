8a57b954dc8e9b634fd71643b22832f4c212e913
Haggai Alehegn <111057970+haggai333@users.noreply.github.com>
2026-05-16 18:52:07 +0300

    Time: 24 ms (9.93%), Space: 13.6 MB (6.29%) - LeetHub

--- 0641-design-circular-deque/0641-design-circular-deque.py
+++ 0641-design-circular-deque/0641-design-circular-deque.py
@@ -1,71 +1,115 @@
-class Node:
-    def __init__(self,val):
-        self.val=val
+class Node():
+    def __init__(self, k):
+    
+        self.val=k
         self.next=None
+        self.prev=None
+
 class MyCircularDeque(object):
-    def __init__(self,k):
+
+    def __init__(self, k):
+        self.max=k
         self.size=0
         self.head=None
-        self.tail=None
-        self.k=k
+        self.tail=self.head
+        
 
-    def insertFront(self,value):
-        if self.size==self.k:
+    def insertFront(self, value):
+        current=Node(value)
+        if self.size==self.max:
             return False
-        node=Node(value)
-        if not self.head:
-            self.head = self.tail=node
+        if self.head:
+            
+            current.next=self.head
+            self.head.prev=current
+            self.head=current
         else:
-            node.next=self.head
-            self.head=node
+            self.head=current
+            self.tail=current
+        self.size+=1
+        return True
+        
+        
+
+    def insertLast(self, value):
+        if self.size==self.max:
+            return False
+        current=Node(value)
+        if self.tail:
+            
+            current.prev=self.tail
+            self.tail.next=current
+            self.tail=current
+        else:
+            self.head=current
+            self.tail=current
         self.size+=1
         return True
 
-    def insertLast(self,value):
-        if self.size==self.k:
-            return False
-        node=Node(value)
-        if not self.head:
-            self.head=self.tail = node
-        else:
-            self.tail.next=node
-            self.tail=node
-        self.size+=1
-        return True
+        
 
     def deleteFront(self):
-        if not self.head:
+        if self.head and self.size!=0:
+            if self.head.next:
+                t=self.head
+                self.head=self.head.next
+                t.next=None
+                self.head.prev=None
+            else:
+                self.head=None
+                self.tail=None
+            self.size-=1
+            return True
+        else:
             return False
-        self.head=self.head.next
-        if not self.head:
-            self.tail=None
-        self.size-=1
-        return True
+        
 
     def deleteLast(self):
-        if not self.head:
-            return False
-        if self.size==1:
-            self.head=self.tail=None
+        if self.tail and self.size!=0:
+            if self.tail.prev:
+                t=self.tail
+                self.tail=self.tail.prev
+                t.prev=None
+                self.tail.next=None
+            else:
+                self.head=None
+                self.tail=None
+            self.size-=1
+            return True
         else:
-            temp=self.head
-            while temp.next!=self.tail:
-                temp=temp.next
-            temp.next=None
-            self.tail=temp
-        self.size-=1
-        return True
+            return False
+        
 
     def getFront(self):
-        return self.head.val if self.head else -1
+        if self.head:
+            return self.head.val
+        else:
+            return -1
+        
+
     def getRear(self):
-        return self.tail.val if self.tail else -1
+        if self.tail:
+            return self.tail.val
+        else:
+            return -1
+        """
+        :rtype: int
+        """
+        
 
     def isEmpty(self):
-        return self.size == 0
+        return self.size==0
+        """
+        :rtype: bool
+        """
+        
 
     def isFull(self):
-        return self.size == self.k
+        return self.size==self.max
+        
+        
+
+
 # Your MyCircularDeque object will be instantiated and called as such:
 # obj = MyCircularDeque(k)
 # param_1 = obj.insertFront(value)
