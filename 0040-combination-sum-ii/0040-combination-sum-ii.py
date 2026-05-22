2c175e0b41d6ab8155b049de6ecfc3ea467955f4
Haggai Alehegn <111057970+haggai333@users.noreply.github.com>
2026-05-22 20:56:06 +0300

    Time: 47 ms (13.53%), Space: 12.6 MB (7.6%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def combinationSum2(self, candidates, target):
+        path=[]
+        answer=[]
+        candidates.sort()
+        def backtrack(currentsum,j):
+            if currentsum==target:
+                answer.append(path[:])
+                return
+            if currentsum>target:
+                return
+            while j<len(candidates):
+                path.append(candidates[j])
+                backtrack(currentsum+candidates[j],j+1)
+                path.pop()
+                while j<len(candidates)-1 and candidates[j]==candidates[j+1]:
+                    j+=1
+                j+=1
+        backtrack(0,0)
+        return answer
+
+
