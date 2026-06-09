af547dd8b27910232df8c6a93ea1e4f7d8b3cef7
Haggai Alehegn <111057970+haggai333@users.noreply.github.com>
2026-06-09 22:53:50 +0300

    Time: 171 ms (53.57%), Space: 13.4 MB (46.71%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def minEatingSpeed(self, piles, h):
+        r=max(piles)
+        r+=r
+        l=1
+        while l<r:
+            mid=(l+r)//2
+            count=0
+            for i in piles:
+                count+=((i+mid-1)//mid)
+                if count>h:
+                    break
+
+            if count>h:
+                l=mid+1
+            else:
+                r=mid
+        return l
+
+
