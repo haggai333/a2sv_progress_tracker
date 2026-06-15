5298551ee29763d606e131a665387300f3d401e7
Haggai Alehegn <111057970+haggai333@users.noreply.github.com>
2026-06-15 23:34:08 +0300

    Time: 332 ms (7.07%), Space: 34 MB (5.62%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -2,29 +2,22 @@ class Solution(object):
     def numIslands(self, grid):
         visited=set()
         answer=0
+        def dfs(i,j):
+            if (i,j) in visited or grid[i][j]=="0":
+                return
+            visited.add((i,j))
+            dirs=[[1,0],[0,1],[-1,0],[0,-1]]
+            for o,p in dirs:
+                x=i+o
+                y=j+p
+                if x>-1 and x<len(grid) and y>-1 and y<len(grid[0]):
+                    dfs(x,y)
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j]!="1" or (i,j) in visited:
                     continue
                 answer+=1
-                visited.add((i,j))
-                stack=[]
-                dirs=[[1,0],[0,1],[-1,0],[0,-1]]
-                for p in dirs:
-                    o=p[0]+i
-                    u=p[1]+j
-                    if o>-1 and o<len(grid) and u>-1 and u<len(grid[0]) and grid[o][u]=="1":
-                        stack.append([o,u])
-                while stack:
-                    x,y=stack.pop()
-                    visited.add((x,y))
-                    for p in dirs:
-                        o=p[0]+x
-                        u=p[1]+y
-                        if (o,u) in visited:
-                            continue
-                        if o>-1 and o<len(grid) and u>-1 and u<len(grid[0]) and grid[o][u]=="1":
-                            stack.append([o,u])
+                dfs(i,j)
         return answer
 
 
