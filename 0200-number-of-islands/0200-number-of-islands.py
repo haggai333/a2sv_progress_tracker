97dd698b75dcb3a61368fad6b1b7ec6384eeb891
Haggai Alehegn <haggaialehegn12@gmail.com>
2026-06-23 00:16:56 +0300

    Time: 231 ms (47.54%), Space: 27.4 MB (98.99%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,21 +1,29 @@
 class Solution(object):
     def numIslands(self, grid):
-        visited=set()
         answer=0
-        def dfs(i,j):
-            if (i,j) in visited or i<0 or i>len(grid)-1 or j<0 or j>len(grid[0])-1 or grid[i][j]=="0":
-                return
-            visited.add((i,j))
-            dirs=[[1,0],[0,1],[-1,0],[0,-1]]
-            for o,p in dirs:
-                dfs(i+o,j+p)
+        def bfs(i,j):
+            queue=deque()
+            queue.append((i,j))
+            dir=[(1,0),(0,1),(-1,0),(0,-1)]
+            while queue:
+                x,y=queue.popleft()
+                 
+                for xi,xy in dir:
+                    n=xi+x
+                    p=xy+y
+                    if n>-1 and n<len(grid) and p>-1 and p<len(grid[0]) and grid[n][p]!="0":
+                        queue.append((n,p))
+                        grid[n][p]="0"
+                         
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j]!="1" or (i,j) in visited:
-                    continue
-                answer+=1
-                dfs(i,j)
+                if grid[i][j]!="0":
+                    answer+=1
+                    bfs(i,j)
+        
         return answer
+
+
 
 
                 
