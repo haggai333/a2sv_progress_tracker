a36705ae062f5a0eadda61eff30fd810687cdf30
Haggai Alehegn <haggaialehegn12@gmail.com>
2026-07-10 15:04:36 +0300

    Time: 12 ms (96.29%), Space: 13.2 MB (61.7%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,13 +1,11 @@
 class Solution(object):
     def canJump(self, nums):
-        i=0
         furthest=0
-        while i<len(nums)-1:
+        for i in range(len(nums)-1):
             if furthest<i:
                 return False
             if furthest<i+nums[i]:
                 furthest=i+nums[i] 
-            i+=1
         return furthest>=len(nums)-1
 
 
