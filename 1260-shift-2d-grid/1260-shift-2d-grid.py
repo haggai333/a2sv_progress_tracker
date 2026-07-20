be5fa757e29e3fda3a5ae3d3e5d23a0656cefa65
Haggai Alehegn <haggaialehegn12@gmail.com>
2026-07-20 22:49:44 +0300

    Time: 168 ms (15.63%), Space: 12.5 MB (80.21%) - LeetHub

---
+++ 1260-shift-2d-grid/1260-shift-2d-grid.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def shiftGrid(self, grid, k):
+        q=deque()
+        m=len(grid)
+        n=len(grid[0])
+        k=k%(m*n)
+        for i in grid:
+            for j in i:
+                q.append(j)
+        grid=[]
+        for i in range(k):
+            q.appendleft(q.pop())
+        
+        while q:
+            a=[]
+            for i in range(n):
+                print(1)
+                a.append(q.popleft())
+            grid.append(a)
+            a=[]
+        return grid
+        
